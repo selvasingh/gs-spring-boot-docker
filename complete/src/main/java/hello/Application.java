06bfa51e12820e26ce6334c50eb45930ca351d2e
Asir Vedamuthu Selvasingh <asirveda@microsoft.com>
2017-06-19 10:41:17 -0700

    print env

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -11,7 +12,20 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        StringBuffer buffer = new StringBuffer("Hello Docker World");
+        buffer.append("<br>");
+        buffer.append("=============================================");
+        buffer.append("<br><br><br>");
+
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            buffer.append(String.format("%s=%s%n",
+                    envName,
+                    env.get(envName)));
+            buffer.append("<br>");
+        }
+
+        return buffer.toString();
     }
 
     public static void main(String[] args) {
